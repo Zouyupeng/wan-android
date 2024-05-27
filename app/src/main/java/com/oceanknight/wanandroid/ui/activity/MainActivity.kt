bc147f9370f24aeac251e2be3fa42f098063da37
Oceanknight <2827280689zou@gmail.com>
2024-05-27 13:19:07 +0800

    :sparkles: feat: 创建底部导航栏
    * 最后一项我的先凑合用着，之后改成用户头像

--- app/src/main/java/com/oceanknight/wanandroid/ui/activity/MainActivity.kt
+++ app/src/main/java/com/oceanknight/wanandroid/ui/activity/MainActivity.kt
@@ -6,10 +6,23 @@ import com.oceanknight.wanandroid.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun initView() {
-
+        initNavigationBar()
     }
 
     override fun initData() {
+
+    }
+
+    /**
+     * 初始化底部导航栏
+     */
+    private fun initNavigationBar() {
+        val iconList = arrayListOf(R.drawable.selector_navigation_home, R.drawable.selector_navigation_search,
+            R.drawable.selector_navigation_collections, R.drawable.selector_navigation_mine)
+
+        iconList.forEach {
+            binding.bottomNavigationBar.addTab(binding.bottomNavigationBar.newTab().setIcon(it))
+        }
 
     }
 
