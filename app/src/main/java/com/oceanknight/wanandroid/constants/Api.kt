66d54f6c31d355c731591548e5b1d06c23099752
Oceanknight <2827280689zou@gmail.com>
2024-07-08 13:18:35 +0800

    :construction: wip(广场): 完成拉取列表的逻辑
    * 和首页的数据格式一样

--- app/src/main/java/com/oceanknight/wanandroid/constants/Api.kt
+++ app/src/main/java/com/oceanknight/wanandroid/constants/Api.kt
@@ -4,8 +4,13 @@ object Api {
     const val HOST = "https://www.wanandroid.com"
     const val SUFFIX = "json"
 
+    // 首页banner 1.2
     const val BANNER = "/banner"
+    // 首页文章列表 1.1
     // 后加页码，页码从0开始
     const val HOME_ARTICLE = "/article/list/"
+    // 置顶文章 1.5
     const val TOP_ARTICLE = "/article/top"
+    // 广场列表数据 10.1
+    const val USER_ARTICLE = "/user_article/list/"
 }
--- app/src/main/java/com/oceanknight/wanandroid/ui/fragment/home/SquareFragment.kt
+++ app/src/main/java/com/oceanknight/wanandroid/ui/fragment/home/SquareFragment.kt
@@ -1,22 +1,39 @@
 package com.oceanknight.wanandroid.ui.fragment.home
 
+import com.drake.brv.utils.setup
+import com.drake.net.Get
+import com.drake.net.utils.scopeNetLife
 import com.oceanknight.wanandroid.R
 import com.oceanknight.wanandroid.base.BaseFragment
+import com.oceanknight.wanandroid.constants.Api
 import com.oceanknight.wanandroid.databinding.FragmentSquareBinding
-import com.oceanknight.wanandroid.databinding.FragmentTopBinding
+import com.oceanknight.wanandroid.net.model.Articles
 
 /**
  *
  * author: Oceanknight
  * date: 2024/05/31
  * describe:
+ * 广场
  */
 class SquareFragment: BaseFragment<FragmentSquareBinding>(R.layout.fragment_square) {
     override fun initView() {
-
+        binding.squareArticleRv.setup {
+            addType<Articles.Article>(R.layout.item_article)
+        }
+        binding.squareArticlePage.onRefresh {
+            fetchSquareData()
+        }
     }
 
     override fun initData() {
+        fetchSquareData()
+    }
 
+    private fun fetchSquareData() {
+        scopeNetLife {
+            val squareArticleRes = Get<Articles>(Api.USER_ARTICLE + (binding.squareArticlePage.index - 1)).await()
+            binding.squareArticlePage.addData(squareArticleRes.articleList)
+        }
     }
 }
